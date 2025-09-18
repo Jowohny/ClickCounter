2f869b94f57e6bdf5aa828fc8704160e8fa5de63
Jowohny <vujohny109238@gmail.com>
2025-09-17 17:45:01 -0700

    time to test

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
-const count = ref(0)
+const count = ref(0);
+let socket: WebSocket | null = null;
+
+
+onMounted(() => {
+  //asspulled this also, didn't feel like switching the url back and forth
+  const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+  const url = `${protocol}//${window.location.host}`;
+
+  // Establish the WebSocket connection.
+  socket = new WebSocket(url);
+
+  socket.onopen = () => console.log('WebSocket connection established.');
+  
+  //this activates everytime a message is received from the server, via the broadcastCount 'method' in the server
+  socket.onmessage = (event) => {
+    //parses numerical data from the server everytime a click is registered and updates the reference to the count, in turn updates the count in the template
+    const data = JSON.parse(event.data);
+    count.value = data.count;
+  };
+  
+  //just closes the connection
+  socket.onclose = () => console.log('WebSocket connection closed.');
+});
 
 const handleIncrement = () => {
-  count.value++
-}
+  //checks if there is a socket available at the URL and also checks if its open, then sends an 'incrememnt' action message to the server which incrememts the count on the server side
+  if (socket?.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify({ action: 'increment' }));
+  }
+};
+
 </script>
 
 <template>
